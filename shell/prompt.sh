cac381ee7539efb69674f81fdb4cae6aaab7dd7f
Felix Sidharta Salim <ravenkelder@gmail.com>
2021-12-03 23:18:45 +1100

    Update prompt.sh, add gitignore

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # Sets up a bash prompt showing the current git branch, and terraform workspace
 # Place in ~/.bash_profile, or as required
 
-no_colour=`tput sgr0`
-green=`tput setaf 40`
-yellow=`tput setaf 3`
-purple=`tput setaf 69`
+no_colour=$(tput sgr0)
+green=$(tput setaf 40)
+yellow=$(tput setaf 3)
+purple=$(tput setaf 69)
 parse_terraform_env() {
   if [ -d ./.terraform ]; then
     if test -f ./.terraform/environment; then
